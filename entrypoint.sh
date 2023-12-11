7c83eedfa13b714813982d98fa75d02d7613d776
rassi0429 <taitokokoa+rassi@gmail.com>
2023-12-12 07:59:26 +0900

    w

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,6 @@ export STEAM_COMPAT_CLIENT_INSTALL_PATH="/home/container/.steam/steam"
 export STEAM_COMPAT_DATA_PATH="/home/container/.steam/steam/steamapps/compatdata/0"
 mkdir -p $STEAM_COMPAT_DATA_PATH
 
-USER container
 
 echo hi
 ls -la /home/container/
